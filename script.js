ce1fc8f884e2b22e4b310d304e409a209e1dde7f
Juan Leonardo Lopez <leolopezedlp@gmail.com>
2023-06-28 17:55:18 -0300

    correccion

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 function agregar(valor) {
     document.getElementById("pantalla").value += valor
     document.getElementById("subpantalla").value += valor
-    subcalcular()
+    if ("." || "+" || "/" || "-" || "*" !== valor ) {
+        subcalcular()
+    }
 }
 
 function borrar() {
     document.getElementById("pantalla").value = "" 
+    document.getElementById("subpantalla").value = "" 
 }
 
 function calcular() {
     const valorPantalla = document.getElementById("pantalla").value
     const resultado = eval(valorPantalla)
     document.getElementById("pantalla").value = resultado
+    document.getElementById("subpantalla").value = ""
 }
 
 function subcalcular() {
     const valorSubpantalla = document.getElementById("subpantalla").value
-    const resultado = eval(valorSubpantalla) 
+    let resultado = eval(valorSubpantalla) 
     document.getElementById("subpantalla").value = resultado
 }
