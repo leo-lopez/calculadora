78a9a161cc01170327767acacd1641dbec6dcf57
Juan Leonardo Lopez <leolopezedlp@gmail.com>
2023-06-28 16:39:39 -0300

    agregue la subpantalla semi funcionando como quiero

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function agregar(valor) {
     document.getElementById("pantalla").value += valor
+    document.getElementById("subpantalla").value += valor
+    subcalcular()
 }
 
 function borrar() {
@@ -10,4 +12,10 @@ function calcular() {
     const valorPantalla = document.getElementById("pantalla").value
     const resultado = eval(valorPantalla)
     document.getElementById("pantalla").value = resultado
+}
+
+function subcalcular() {
+    const valorSubpantalla = document.getElementById("subpantalla").value
+    const resultado = eval(valorSubpantalla) 
+    document.getElementById("subpantalla").value = resultado
 }
