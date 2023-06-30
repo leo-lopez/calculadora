f3f3a176d21eac3bd7a5ce39371ebc01317a6479
Juan Leonardo Lopez <leolopezedlp@gmail.com>
2023-06-30 12:12:24 -0300

    pregresando un poco

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
 function agregar(valor) {
+    if (document.getElementById("subpantalla").value === "") {
+            document.getElementById("subpantalla").value =  document.getElementById("pantalla").value
+    }
     document.getElementById("pantalla").value += valor
-    document.getElementById("subpantalla").value += valor
-    if ("." || "+" || "/" || "-" || "*" !== valor ) {
+    document.getElementById("subpantalla").value = document.getElementById("pantalla").value
+    if (valor !== '.' && valor !== '+' && valor !== '/' && valor !== '-' && valor !== '*'  ) {
         subcalcular()
+        console.log(document.getElementById("pantalla").value)
     }
 }
 
 function borrar() {
     document.getElementById("pantalla").value = "" 
-    document.getElementById("subpantalla").value = "" 
+    document.getElementById("subpantalla").innerHTML = "" 
 }
 
 function calcular() {
     const valorPantalla = document.getElementById("pantalla").value
     const resultado = eval(valorPantalla)
     document.getElementById("pantalla").value = resultado
-    document.getElementById("subpantalla").value = ""
+    document.getElementById("subpantalla").innerHTML = ""
+
 }
 
 function subcalcular() {
-    const valorSubpantalla = document.getElementById("subpantalla").value
-    let resultado = eval(valorSubpantalla) 
-    document.getElementById("subpantalla").value = resultado
+    const valorSubpantalla = document.getElementById("pantalla").value
+    const resultado = eval(valorSubpantalla) 
+    document.getElementById("subpantalla").innerHTML = resultado
 }
